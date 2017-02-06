5957d18bfe46f46a7db561a79391551a0eff6c82
Myst <tormyst@github.com>
2017-02-05 23:36:25 -0400

    Upgrade to C++

---
+++ database.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Raphael Bronfman-Nadas on 2017-02-05.
+//
+
+#include "database.h"
---
+++ database.h
@@ -0,0 +1,14 @@
+//
+// Created by Raphael Bronfman-Nadas on 2017-02-05.
+//
+
+#ifndef DATA_A3_DATABASE_H
+#define DATA_A3_DATABASE_H
+
+
+class database {
+
+};
+
+
+#endif //DATA_A3_DATABASE_H
