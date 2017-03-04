83a75245021a6e8a4667d9d99c3d9dcdc2b658c4
Myst <tormyst@github.com>
2017-03-04 14:39:54 -0400

    Fixing up the rule combine function. Almost the exact same as the the other function.  Not sure why I coped so much of it here before.

--- src/rule.cpp
+++ src/rule.cpp
@@ -14,19 +14,14 @@ Rule::Rule(std::shared_ptr<Database> db, FrequentSet tail, FrequentSet combined)
 
 double Rule::getConfidence() const {return confidence;}
 double  Rule::getFrequencyCount() const {return combined.getFrequency();}
-// Very similer to the frequency set combine, this join acts on the tail lists, and tries to combine them.
-// TODO check if I can just remove the bulk of this.
+// Checks if there are enough filters to move one to the tail. Checks if the tails combine.
+// We are good.  Valid rule then.
 Rule Rule::combine(std::shared_ptr<Database> db, Rule other) const {
-    std::vector<intpair> tailList = this->tail.getRawFilters();
-    std::vector<intpair>  otherTailList = other.tail.getRawFilters();
-    if( tailList.size() != otherTailList.size() || head.size() < 2)
+    FrequentSet f;
+    if( head.size() < 2)
         return Rule(); // Null value
-    int i;
-    for(i = 0; i < tailList.size() - 1; i++){
-        if(tailList[i] != otherTailList[i])
-            return Rule(); // Null value
-    }
-    if(tailList[i] == otherTailList[i] || tailList[i].first == otherTailList[i].first)
+    f = tail.combine(other.tail);
+    if(FrequentSet::isNull(f))
         return Rule(); // Null value
     return Rule(db, tail.combine(other.tail), combined);
 }
